ecd8719ab1f47494593be297eda2615715800f20
Ban-gilhyeon <bbgiloo98@gmail.com>
2024-11-12 01:34:35 +0900

    feat :

---
+++ discovery_service_MSA_Study/src/main/java/com/example/discovery_service_msa_study/DiscoveryServiceMsaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.discovery_service_msa_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceMsaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceMsaStudyApplication.class, args);
+	}
+
+}
