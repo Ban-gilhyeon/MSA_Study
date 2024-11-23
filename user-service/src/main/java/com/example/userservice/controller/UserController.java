50a52f70b09e73243d96f1d7ee984e456cc6e5c0
Ban-gilhyeon <bbgiloo98@gmail.com>
2024-11-24 05:01:42 +0900

    user-serivce 사용자 조회2

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("user-service")
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
